abdc8b61020d4b5f27e2e0c9fcb879fdba3f3d20
NurbekSaitqulov03 <nurbeksaitqulov747@gmail.com>
2023-07-24 09:18:32 +0500

    add

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from tinydb import TinyDB
+from api import r
+db = TinyDB('data.json', indent=4)
 
-db = TinyDB('data.json')
+if r.status_code == 200:
+    user = r.json()
+
+    for i in user['results']:
+        db.insert({
+            "first_name":i["name"]['first'],
+            "last_name": i['name']['last'],
+            "age": i['dob']['age'],
+        })
+
